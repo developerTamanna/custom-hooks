2c47069b0088ba374e3bd1e1a1d12b6bfd1dff43
developerTamanna <astamanna57@gmail.com>
2025-04-17 12:42:08 +0600

    extra function encr decre

--- src/components/Counter1/Counter1.jsx
+++ src/components/Counter1/Counter1.jsx
@@ -3,13 +3,27 @@ import React, { useState } from 'react';
 const Counter1 = () => {
 
     const [counter, setCounter] = useState(0)
+
+//increment function
+
+const increment = ()=>{
+    setCounter(counter+1)
+}
+
+//decrement function
+
+const decrement = ()=>{
+    setCounter(counter-1)
+}
+
+
     return (
         <div >
             <h1 className='ml-24 text-3xl font-semibold'>{counter}</h1>
          <div className='flex gap-4 mt-10'>
-         <button
-          onClick={()=>setCounter(counter+1)} className='btn'>increment</button>
-         <button onClick={()=>setCounter(counter-1)} className='btn'>decrement</button>
+         <button onClick={increment}
+           className='btn'>increment</button>
+         <button onClick={decrement} className='btn'>decrement</button>
           </div>
           <br></br>
          
